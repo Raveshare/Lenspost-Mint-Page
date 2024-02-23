2c72732bdf23ec1ef071a2b94e4b0c2c89a01759
aryan <agarwalaryan139@gmail.com>
2024-02-23 21:02:29 +0530

    test: sol

--- src/pages/components/NFTCard.tsx
+++ src/pages/components/NFTCard.tsx
@@ -30,6 +30,7 @@ export default function NFTCard() {
       },
     },
   ];
+
   return (
     <div className="p-6 sm:p-10 bg-white shadow-2xl flex flex-col sm:flex-row justify-between rounded-3xl max-w-4xl mx-auto gap-8">
       <img
@@ -39,7 +40,7 @@ export default function NFTCard() {
       />
       <div className="w-full">
         <div className="w-fit ml-auto">
-          <CustomConnectButton isEVM />
+          <CustomConnectButton isEVM={false} />
         </div>
         <div className="flex items-center justify-between mt-6">
           <h3 className="text-xl sm:text-4xl font-semibold">LensPost 2024</h3>
@@ -65,6 +66,36 @@ export default function NFTCard() {
             Feb 16, 2024 00:00 (UTC) - Feb 04, 2024 23:59 (UTC)
           </p>
         </div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <div
+              className="p-2 rounded-lg border-2 border-[#9291A3] text-[#5E5C8D] font-semibold text-xs my-2 flex items-center max-w-96 justify-between"
+              aria-label="Customise options"
+            >
+              <p>Select Network</p>
+              <div className="flex items-center gap-2">
+                <p>{network}</p>
+                <ArrowDown color="#5E5C8D" width={16} height={16} />
+              </div>
+            </div>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="min-w-96 bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+              sideOffset={5}
+            >
+              {Networks.map((network, index) => (
+                <DropdownMenu.Item
+                  className="cursor-pointer hover:bg-gray-100 p-2 leading-none text-violet11 rounded-[3px] flex items-center select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                  key={index}
+                  onClick={network.onclick}
+                >
+                  {network.name}
+                </DropdownMenu.Item>
+              ))}
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
         <DropdownMenu.Root>
           <DropdownMenu.Trigger asChild>
             <div
