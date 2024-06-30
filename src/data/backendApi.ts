77de156662d1c6e4f25b2029b023c21aa468a48e
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-06-30 20:37:03 +0530

    fix: backend URL

--- src/data/backendApi.ts
+++ src/data/backendApi.ts
@@ -1,5 +1,5 @@
 export const BACKEND_API = Object.freeze({
   production: 'https://api-v2.poster.fun',
   development: 'https://dev.poster.fun',
-  local: 'http://localhost:3001'
+  local: 'https://dev.poster.fun'
 });
